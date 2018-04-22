ce693622ccb8d35e4c873104fe00f6444c867712
Roosevelt <panta_85.x@hotmail.com>
2018-04-22 12:13:40 -0500

    modified main activity (fragments)

--- app/src/main/java/com/example/roosevelt/imagesbottomnavbar/viewcontrollers/activities/MainActivity.java
+++ app/src/main/java/com/example/roosevelt/imagesbottomnavbar/viewcontrollers/activities/MainActivity.java
@@ -3,33 +3,23 @@ package com.example.roosevelt.imagesbottomnavbar.viewcontrollers.activities;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 import com.example.roosevelt.imagesbottomnavbar.R;
+import com.example.roosevelt.imagesbottomnavbar.viewcontrollers.fragments.HomeFragment;
+import com.example.roosevelt.imagesbottomnavbar.viewcontrollers.fragments.ImagesFragment;
+import com.example.roosevelt.imagesbottomnavbar.viewcontrollers.fragments.UploadFragment;
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_images:
-                    mTextMessage.setText(R.string.title_images);
-                    return true;
-                case R.id.navigation_upload:
-                    mTextMessage.setText(R.string.title_upload);
-                    return true;
-            }
-            return false;
+            return navigateTo(item);
         }
     };
 
@@ -38,9 +28,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        navigateTo(navigation.getMenu().findItem(R.id.navigation_home));
+    }
+
+    private Fragment getFragmentFor(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.navigation_home: return new HomeFragment();
+            case R.id.navigation_images: return new ImagesFragment();
+            case R.id.navigation_upload: return new UploadFragment();
+            default: return new HomeFragment();
+        }
+    }
+
+    private boolean navigateTo(MenuItem item) {
+        item.setChecked(true);
+        return getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.content, getFragmentFor(item))
+                .commit() > 0;
     }
 
 }
