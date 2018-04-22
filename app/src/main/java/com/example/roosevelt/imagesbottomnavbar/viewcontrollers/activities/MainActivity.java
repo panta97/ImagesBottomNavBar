2f3c237a99af781053133eb4011cc6733de9ef71
Roosevelt <panta_85.x@hotmail.com>
2018-04-22 11:59:31 -0500

    fragments

--- app/src/main/java/com/example/roosevelt/imagesbottomnavbar/viewcontrollers/activities/MainActivity.java
+++ app/src/main/java/com/example/roosevelt/imagesbottomnavbar/viewcontrollers/activities/MainActivity.java
@@ -22,11 +22,11 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.navigation_home:
                     mTextMessage.setText(R.string.title_home);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_images:
+                    mTextMessage.setText(R.string.title_images);
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_upload:
+                    mTextMessage.setText(R.string.title_upload);
                     return true;
             }
             return false;
